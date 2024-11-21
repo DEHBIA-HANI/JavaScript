deadb8add9eb40f4690f976aa13b88f0b515f158
DEHBIA-HANI <129846731+DEHBIA-HANI@users.noreply.github.com>
2024-11-21 11:24:25 +0100

    les variables '1.string'

---
+++ script.js
@@ -0,0 +1,7 @@
+//Les variables en JavaScript
+//Variables "modifiable"
+let myName = "Dehbia";
+console.log("name", myName);
+myName = "louise";
+console.log("name", myName);
+//Variable "non modifiable"
